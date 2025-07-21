3df916a85ac2e5920eb9bdcae30e13a85f5f3557
Nikolay <nikolay.tetradov@gmail.com>
2025-07-21 14:18:51 +0300

    tons of changes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Y2mo1BrGXWdlSQCrKDCXswqXQzgxIeehvFwReKmONT0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
